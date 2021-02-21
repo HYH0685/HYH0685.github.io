73e0a8b5f51d1d9907d702f95d792a0b98c73938
Eurkon <1124724692@qq.com>
2021-02-21 23:23:44 +0800

    Site updated: 2021-02-21 23:23:43

--- js/history.js
+++ js/history.js
@@ -1,37 +1,69 @@
-var myDate = new Date();
+function append (parent, text) {
+  if (typeof text === 'string') {
+    var temp = document.createElement('div');
+    temp.innerHTML = text;
+    // 防止元素太多 进行提速
+    var frag = document.createDocumentFragment();
+    while (temp.firstChild) {
+      frag.appendChild(temp.firstChild);
+    }
+    parent.appendChild(frag);
+  }
+  else {
+    parent.appendChild(text);
+  }
+}
 
-var myMonth = myDate.getMonth() + 1;
-getMonth = myMonth < 10 ? ("0" + String(myMonth)) : String(myMonth);
+function history_get_data () {
+  let myDate = new Date();
+  let myMonth = myDate.getMonth() + 1;
+  if (myMonth < 10) {
+    getMonth = "0" + String(myMonth);
+  } else {
+    getMonth = String(myMonth);
+  }
+  let getDate = String(myDate.getDate());
+  if (getDate < 10) {
+    getDate = "0" + String(getDate);
+  } else {
+    getDate = String(getDate);
+  }
+  let getMonthDate = "S" + getMonth + getDate;
+  return ["https://cdn.jsdelivr.net/gh/Eurkon/CDN/hexo/json/history/" + getMonth + ".json", getMonthDate]
+}
 
-var getDate = String(myDate.getDate());
-getDate = getDate < 10 ? ("0" + String(getDate)) : String(getDate);
-
-var getMonthDate = "S" + getMonth + getDate;
-var geturl = "https://cdn.jsdelivr.net/gh/Eurkon/CDN/hexo/json/history/" + getMonth + ".json";
-
-btf.isJqueryLoad(() => {
-  $(function () {
-    $.ajax({
-      //请求方式
-      type: "GET",
-      //文件位置
-      url: geturl,
-      //返回数据格式为json,也可以是其他格式
-      dataType: "json",
-      //请求成功后要执行的函数，拼接html
-      success: function (data) {
-        var str = '<ul style="list-style: none;padding-inline-start: 0px;">';
-        $.each(data[getMonthDate], function (i, n) {
-          str += '<li style="list-style: none;height:100px;">';
-          str += '<p style="color:#858585; Font-style:italic; font-weight:lighter; float:left; width:100%; font-size: 14px; margin:0px; padding:0px; display:inline-block">';
-          str += "<i>A.D.</i>" + n.year + "</p>";
-          str += '<p style="width:100%; float:left; margin:0px; padding:0px; display:inline-block">';
-          str += n.title + '<i style="Font-style:normal;">。</i>';
-          str += "</p>" + "</li>";
-        });
-        str += "</ul>";
-        $("#history-card").append(str);
-      }
-    });
+let history_data = history_get_data()
+fetch(history_data[0]).then(data => data.json()).then(data => {
+  html_item = ''
+  for (let item of data[history_data[1]]) {
+    html_item += '<div class="swiper-slide history_slide"><span class="history_slide_time">A.D.' +
+      item.year + '</span>' + '<span class="history_slide_link">' + item.title + '</span></div>'
+  }
+  let history_container_wrapper = document.getElementById('history_container_wrapper')
+  append(history_container_wrapper, html_item);
+  let swiper_history = new Swiper('.history_swiper-container', {
+    passiveListeners: true,
+    spaceBetween: 30,
+    effect: 'coverflow',
+    coverflowEffect: {
+      rotate: 30,
+      slideShadows: false,
+    },
+    loop: true,
+    direction: 'vertical',
+    autoplay: {
+      disableOnInteraction: true,
+      delay: 5000
+    },
+    mousewheel: false,
+    // autoHeight: true,
   });
-});
+
+  let history_comtainer = document.getElementById('history-container');
+  history_comtainer.onmouseenter = function () {
+    swiper_history.autoplay.stop();
+  };
+  history_comtainer.onmouseleave = function () {
+    swiper_history.autoplay.start();
+  }
+})
